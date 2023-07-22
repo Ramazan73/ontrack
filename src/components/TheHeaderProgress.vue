61c3c35191f8a0e8c64d207e02bb76f080b44bc0
Ramazan <rmustakimov@neklo.com>
2023-07-22 11:51:32 +0400

    router refactoring

--- src/components/TheHeaderProgress.vue
+++ src/components/TheHeaderProgress.vue
@@ -1,9 +1,10 @@
 <script setup>
 import { CheckCircleIcon } from '@heroicons/vue/24/solid'
 import { PAGE_PROGRESS } from '@/constants'
+import { navigate } from '@/router'
 </script>
 <template>
-  <a :href="`#${PAGE_PROGRESS}`" class="text-sm">
+  <a @click="navigate(PAGE_PROGRESS)" :href="`#${PAGE_PROGRESS}`" class="text-sm">
     <div v-if="false" class="flex items-center gap-1">
       Day Complete!
       <CheckCircleIcon class="h-7 text-green-500" />
--- src/components/TheLogo.vue
+++ src/components/TheLogo.vue
@@ -1,9 +1,10 @@
 <script setup>
+import { navigate } from '@/router'
 import { PAGE_TIMELINE } from '@/constants'
 </script>
 
 <template>
-  <a :href="`#${PAGE_TIMELINE}`">
+  <a @click="navigate(PAGE_TIMELINE)" :href="`#${PAGE_TIMELINE}`">
     <img src="../assets/img/logo.png" alt="Logo" class="h-9" />
   </a>
 </template>
